821fcd2bd3ba08a2c9fe7952d1399751071be412
IT24100084 <it24100084@my.sliit.lk>
2025-05-17 15:34:15 +0530

    Delete OrderRepository.java

--- OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fooddelivery.orderbackend.repository;
-
-import com.fooddelivery.orderbackend.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
